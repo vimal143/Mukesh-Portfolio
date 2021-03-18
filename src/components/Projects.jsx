0eff85c6d45459a58dbfbe2e3d9ef68f485b57c8
vimal143 <vimalpandey211@gmail.com>
2021-03-18 20:38:30 +0530

    Project link added

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -11,22 +11,27 @@ const Projects = () => {
                 </div>
                 <div className="row">
                     <div className="column">
+                        
                         <div className="card">
+                        <a href="http://campussathi.in">
                             <img src="/images/Project_image1.png" alt="Avatar" className="image" />
                             <div className="container">
                                 <h4><b>CampusSathi</b></h4>
                             </div>
+                            </a>
                         </div>
 
                     </div>
                 
                     <div className="column">
                         <div className="card">
+                            <a href="#">
                             <img src="/images/Home.png" alt="Avatar" className="image" />
                             <div className="container">
                                 <h4><b>Axis Exam Portal</b></h4>
                                 
                             </div>
+                            </a>
                         </div>
 
                     </div>
